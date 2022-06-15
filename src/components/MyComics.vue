56b02991155f632bb1f7d6716ab74649529e4b44
andrea scacchi <andrea.scacchi@live.com>
2022-06-15 23:39:14 +0200

    Completed Exercise, adding sub compenent using props.

---
+++ src/components/MyComics.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="comix">
+        <div v-for="(detail, i) in details" :key="i" class="comic">
+            <img :src="detail.thumb" :alt="detail.series">
+            <h5>{{ detail.series }}</h5>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'MyComics',
+  props: {
+    details: Object
+  }
+}
+</script>
